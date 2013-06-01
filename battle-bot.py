4d38c46d3e9bee0713bd1c7f50f6110a8cdda9f7
Andrew Spano <andrew@localhost.localdomain>
2013-05-31 23:13:54 -0400

    abstracted out irc logic into its own file

--- battle-bot.py
+++ battle-bot.py
@@ -3,9 +3,9 @@ import os
 import sys
 import string
 
-readbuffer=""
+
 backsock = socket.socket() # socket to connect to the web back end
-ircsock = socket.socket() # socket to connect to irc channel
+
 
 def init_server_connection ():
     "Connects client bot to back-end server"
@@ -15,20 +15,20 @@ def init_server_connection ():
 
     backsock.connect((backhost,backport))
 
-def init_irc_connection ():
-    irchost = "mccs.stu.marist.edu"
-    ircport = 6667
-    NICK="battle-bot"
-    IDENT="battle-bot"
-    REALNAME="Your Mom"
-    CHAN="#chat"
-    ircsock.connect((irchost,ircport))
-    ircsock.send("NICK %s\r\n" % NICK)
-    ircsock.send("USER %s %s bla :%s\r\n" % (IDENT, irchost,REALNAME))
-    ircsock.send("JOIN:%s\r\n" % CHAN)
-    ircsock.send("PRIVMSG %s:%s\r\n" % (CHAN, "This is a test message. Prepare for imminent mortal combat"))
 
 
+
+while 1:
+    readbuffer=readbuffer+ircsock.recv(1024)
+    temp=string.split(readbuffer,"\n")
+    readbuffer=temp.pop()
+
+for line in temp:
+    line=string.rstrip(line)
+    line=string.split(line)
+
+    if(line[0]=="PING"):
+      ircsock.send("PONG %s\r\n" % line[1])
 
 
     #while 1:
