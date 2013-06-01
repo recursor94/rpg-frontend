8337e281fe58dd9c6efddf431b9662e446ceb73c
Andrew Spano <andrew@localhost.localdomain>
2013-05-31 22:05:54 -0400

    made host depend on env variables

--- batle-bot.py
+++ batle-bot.py
@@ -3,12 +3,12 @@ import os
 import sys
 
 s = socket.socket()
-host = socket.gethostname()
+host = os.environ['HOSTNAME']
 port = 9001
 
 s.connect((host,port))
 
 while 1:
-    s.send("Hello\n")
+    s.send('Hello\n')
     print s.recv(1024)
 s.close()
