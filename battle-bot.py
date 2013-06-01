721e972aca6dbc6d643adf2e552b1f6d7f376c31
Andrew Spano <andrew@localhost.localdomain>
2013-05-31 22:16:00 -0400

    renamed socket of backend to a more descriptive name and added a socket to the irc; also added string library to import

--- battle-bot.py
+++ battle-bot.py
@@ -1,12 +1,17 @@
 import socket
 import os
 import sys
+import string
 
-s = socket.socket()
-host = os.environ['HOSTNAME']
-port = 9001
+backsock = socket.socket() # socket to connect to the web back end
+backhost = os.environ['HOSTNAME']
+backport = 9001
 
-s.connect((host,port))
+backsock.connect((backhost,backport))
+
+# Socket to connect
+ircsock = socket.socket()
+irc
 
 while 1:
     s.send('Hello\n')
